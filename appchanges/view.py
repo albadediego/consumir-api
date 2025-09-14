afacc314194d49a566e03978c69440df0c72b137
Alba <albadediegocorrea@gmail.com>
2025-09-14 18:17:31 +0200

    ultimos cambios mvc

--- appchanges/view.py
+++ appchanges/view.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class ViewCoins:
     def insertCoin(self):
         moneda = input("Ingrese un codigo de moneda: ").upper()
@@ -7,8 +9,8 @@ class ViewCoins:
         print("Rates: ", modelo.respuesta.get('quotes'))
         value1 = modelo.moneda+'EUR'
         value2 = modelo.moneda+'USD'
-        print("EUR: ","{:.2F}€".format(modelo.respuesta['quotes'][value1]))
-        print("USD: ","{:.2F}$".format(modelo.respuesta['quotes'][value2]))
+        datos = f"EUR: { '{:.2f}€'.format(modelo.respuesta['quotes'][value1]) }\nUSD: { '{:.2f}$'.format(modelo.respuesta['quotes'][value2]) }\nFecha: { datetime.fromtimestamp(modelo.respuesta.get('timestamp')) }\nMoneda: { modelo.respuesta.get('source') }"
+        print(datos)
 
     def getError(self, error):
         print(error)
